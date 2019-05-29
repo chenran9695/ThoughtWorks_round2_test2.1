ef0f790f2c487c8b7adb38f292e864929e238234
chenran <chenran_uestc@163.com>
2019-05-29 12:07:08 +0800

    添加了main.js的函数代码，用于求余数

--- main/main.js
+++ main/main.js
@@ -1,4 +1,9 @@
 // Write your cade below:
-module.exports = function main() {
-  
+module.exports = function main(number1,number2) {
+    //被除数不能为零
+    if(number2 == 0){
+        console.log("被除数不能为0！")
+        return Number.NaN;
+    }
+    return number1 % number2;
 };
